63575703a8071e063f7c4e744b14f990cdb7a57b
Jon Penton <jon@spotsource.com>
2024-10-07 12:38:32 -0400

    style: :art: sorted/cleaned up formatting of comments/variables

--- config/environment.bash
+++ config/environment.bash
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-# place all `export` keyword declarations at the start for bash v3 compatibility:
+# Used by `setup-environment-commands`
+# place all `export` keyword declarations at the start for bash
+# v3 compatibility:
 # shellcheck disable=SC2034
 export AWS_CLI_AUTO_PROMPT HOMEBREW_ARCH HOMEBREW_RUBY_VERSION NVM_DIR
 
-# Used by `setup-environment-commands`
-
-NVM_DIR="$HOME/.nvm"
-# ^ Used by: setup-environment-commands, setup-node
-
-HOMEBREW_ARCH='x86_64' # 'arm64e'
-# ^ If you are on Apple Silicon, use 'x86_64' to have Homebrew installed and run as if it were on Apple Intel.
-# ^ Used by: setup-environment-commands, setup-mac-brew, brew
-
-HOMEBREW_RUBY_VERSION='default'
-# ^ Used by: setup-environment-commands, setup-ruby
-
+# Used by: `aws`
 AWS_CLI_AUTO_PROMPT='on'
+
+# Used by: `brew`, `setup-environment-commands`, `setup-mac-brew`
+# If you are on Apple Silicon, use 'x86_64' to have Homebrew installed and run
+# as if it were on Apple Intel.
+HOMEBREW_ARCH='x86_64' # 'arm64e'
+
+# Used by: `setup-environment-commands`, `setup-ruby`
+HOMEBREW_RUBY_VERSION='default'
+
+# Used by: `setup-environment-commands`, `setup-node`
+NVM_DIR="$HOME/.nvm"
--- config/git.bash
+++ config/git.bash
@@ -5,14 +5,17 @@
 # Used by `setup-git`
 # You can use `setup-git --configure` to configure these.
 
-# GPG_SIGNING_AGENT=''
-# GPG_SIGNING_KEY='' # 'op' for 1password, 'krypton' for Krypt.co, 'default' for system default
-# SSH_IDENTITY_AGENT='' # 'op' for 1password, 'krypton' for Krypt.co, 'default' for system default
-
 GIT_DEFAULT_BRANCH='main'
 GIT_EMAIL='jon@spotsource.com'
 GIT_NAME='Jon Penton'
 GIT_PROTOCOL='ssh'
 GITHUB_USERNAME='jon-spotsource'
 # GITLAB_USERNAME=''
+# GPG_SIGNING_AGENT=''
+
+# 'op' for 1password, 'krypton' for Krypt.co, 'default' for system default
+# GPG_SIGNING_KEY=''
 MERGE_TOOL='delta'
+
+# 'op' for 1password, 'krypton' for Krypt.co, 'default' for system default
+# SSH_IDENTITY_AGENT=''
